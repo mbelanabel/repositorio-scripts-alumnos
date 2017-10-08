4bee4f533a9ced92a2ed9bc41091dfd0f2598d9e
jtondadi <32275181+jtondadi@users.noreply.github.com>
2017-10-08 06:39:48 +0200

    Add files via upload

---
+++ prueba13.sh
@@ -0,0 +1,19 @@
+# jt. Octubre 2017
+# ver cuantos dispositivos caracter se encuentran en /dev
+# ver .... bloque
+dir=`pwd`
+caracter=0
+bloque=0
+cd /dev
+lista_archivos=`ls`
+for comprobar_archivo in $lista_archivos
+do 
+ if test -c $comprobar_archivo ; then 
+  caracter=`expr $caracter + 1`
+  echo "dispositivo caracter nº$caracter  : $comprobar_archivo"
+ elif test -b $comprobar_archivo ; then
+  bloque=`expr $bloque + 1`
+  echo "dispositivo bloque nº$bloque : $comprobar_archivo"
+fi
+done
+cd $dir
