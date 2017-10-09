132ae0a73e3b020ee559a42c0e82918016a415e3
Alberto2ASIR <32488127+Alberto2ASIR@users.noreply.github.com>
2017-10-09 17:20:35 +0200

    Script de prueba

---
+++ pruebascript.sh
@@ -0,0 +1,33 @@
+#Alberto Jimenez Martinez 
+#Este script nos permitirá generar un listado de todos los ficheros del
+#directorio root en segundo plano, mostrar un listado de todos los usuarios del
+#sistema(existentes) y mostrar el usuario actual
+#!/bin/bash
+echo "Eliga una opcion"
+echo ""
+echo "1) Generar el listado de todos los ficheros que contiene el directorio root en segundo plano"
+echo "2) Mostrar un listado de todos los usuarios del sistema existentes"
+echo "3) Mostrar el usuario actual"
+echo "4) Salir"
+
+read opcion
+case $opcion  in
+
+	 1)	echo "Listado de todos los ficheros del directorio actual en segundo plano, en la ruta /home/alumno/listado_c"
+		ls -alFR >> /home/alumno/listado_c &
+		;;
+
+	 2)     echo "Muestra un listado de los usuarios existentes del sistema"  
+                finger
+                ;;
+
+	 3)     echo "El usuario actual es"  
+                whoami
+                ;;
+
+	 4)	echo "Salir"
+		break
+		;;
+
+esac
+
