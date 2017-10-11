4ccb311fdd04dd8ce7acf02a173fed1674163e2c
mayco97 <32488107+mayco97@users.noreply.github.com>
2017-10-11 19:02:38 +0200

    Add files via upload

--- script_mayco.sh
+++ script_mayco.sh
@@ -1,15 +1,18 @@
 #!/home/alumno
 #Redacta un script que compruebe si esta en el directorio /, si es así muestra los permisos que tienen los ficheros que contiene, si no, cambia de directorio /.
-
-#Hecho por Mayco Costa Bustamanter
+#Autor Mayco Costa
+#Revisado por Andrea Rioja
+#(1)Fallo, Tiene que ir al directorio raiz para que funcione
 
 echo "Aqui muestra el directorio / y los permisos"
 #ls -l ; pwd
 #cambiar de directorio
 directorio=`pwd`
 if [ $directorio = "/" ] ; then
+#Aquí esta diciendo si estoy en el directorio o no, ya que sino es porque antes tengo que situarme en el directorio / para que funcione.
  	echo "Si estas en el directorio $directorio "
         ls -l 
 else
  	echo "No estas en el directorio $directorio "
 fi
+
