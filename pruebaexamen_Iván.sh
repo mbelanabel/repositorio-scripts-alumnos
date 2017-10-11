0fbac55d5a5ee282eadd92a5c95f0b8153e2b2e0
Racksok <32488015+Racksok@users.noreply.github.com>
2017-10-11 19:05:21 +0200

    Add files via upload

---
+++ pruebaexamen_Iván.sh
@@ -0,0 +1,44 @@
+#Script que por medio de un menú, permitirá:
+#1.- Saber cuántos ficheros de dispotivo tipo carácter hay en /dev
+#2.- Ver cuáles son y lo que ocupan los perfiles de usuario.
+#3.- Crear un informe con esa información
+#4.- Salir
+#!/bin/bash
+#Autor: Iván
+ficheros(){
+	find /dev/* -type c | wc -l
+}
+usuarios(){
+du -sh /home
+}
+informe (){
+echo "la carpeta home del usuario ocupa "
+du -sh /home > informe.txt
+}
+pausa(){
+	echo " "
+	read -n 1 -p "Presiona una tecla para continuar..."
+	}
+menu (){
+PS3="Elija una opción : "
+select opt in Listar usuarios informe salir
+	do
+	if [ "$opt" = "Listar" ]; then
+	ficheros	
+	pausa
+	elif [ "$opt" = "usuarios" ]; then
+	usuarios
+	pausa
+	elif [ "$opt" = "informe" ]; then
+	informe
+	pausa
+	elif [ "$opt" = "salir" ]; then
+	echo "Adios!"
+	exit
+	fi
+	done
+pausa
+menu
+}
+
+menu
