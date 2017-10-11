2d1a6a53985405948fbe1a6e98d960e3f561d114
jtondadi <32275181+jtondadi@users.noreply.github.com>
2017-10-11 19:52:30 +0200

    Add files via upload

--- simulacro01_JuanTonda.sh
+++ simulacro01_JuanTonda.sh
@@ -10,6 +10,11 @@
 # 3. Cuántos ficheros hay en /mnt
 # 4. Listar los ficheros; 10 segundos después, guardar en un archivo
 #
+# revision por Adrian 
+# funcion: listar_archivos
+# en la variable ls se guarda el listado contando el total con wc -w        
+# contar palabras (wc -w) -> se graba en variable total
+
 listar_archivos()
  {
     opcion=0
@@ -24,7 +29,7 @@ dir_actual=`pwd`
 opcion=0
 while [ $opcion -eq 0 ] ; do
 clear
-echo "** MENU ** Agenda **"
+echo "** MENU ****"
 echo " "
 echo "1.- CREAR UN GRUPO"
 echo " "
@@ -42,12 +47,15 @@ case $opcion in
     opcion=0
     clear 
     read -p "Qué grupo quieres crear ? " grupo 
-    groupadd $grupo
+    # groupadd $grupo
+    # esta comentado para no realizarse en maquina real
     ;;
   2) 
     opcion=0
     clear 
     read -p "Qué usuario quieres dar de alta ?" usuario
+    # adduser $usuario
+    # no estaba incluido
     ;; 
   3)
     op=3
@@ -60,11 +68,12 @@ case $opcion in
     cd $dir_actual 
     sleep 10
     echo "Procediendo a sacar la lista de archivos ..."
+    # verifico que el contenido del archivo no tiene tamaño 0 
     if test -s lista_archivos.txt ; then 
       cat lista_archivos.txt
-     else 
+     else
       echo "Archivo sin datos"
-    fi 
+    fi
     read -p "Pulsa una tecla para salir " pausa
     ;;
   5) 
--- simulacro02_JuanTonda.sh
+++ simulacro02_JuanTonda.sh
@@ -7,23 +7,40 @@
 # JUAN TONDA. Octubre 2017. simulacro02 /arrays
 clear
 contador=0
+# revisado por Adrian, añadiendo nuevos comentarios
+# y controlando que exista el nombre en el array
+# se declaran los arrays
 declare -a usuario=( Alberto Roberto Laura Sergio Cristian Dani )
 declare -a telefonos=( 911 811 711 611 511 411 )
 echo "Datos cargados en el array"
 clear
+# mostramos los valores del array de los amigos 
+# contador le vamos dando el valor desde 0 
 for valor in ${usuario[*]}
  do 
    echo $valor 
    contador=`expr $contador + 1`
  done
+# solicitamos el amigo para mostrarnos su teléfono
 read -p " Dime el nombre del amigo : " amigo
+
+# vamos a comprobar el array del telefono, basando en la información
+# del array de los amigos, y una vez localizado mostramos el teléfono
+# con el contador nos situamos en el valor adecuado
+# y con IF verificamos que existe el amigo, guardando el teléfon para
+# su visualización posterior
 contador=0
 for valor in ${usuario[*]}
  do
    if [ $valor = $amigo ]; then 
-    telefono=${telefonos[$contador]}
+     telefono=${telefonos[$contador]}
+    else
+     telefono=0
    fi
    contador=`expr $contador + 1`
  done
- echo "el telefono de $amigo es $telefono"
-
+ if [ $telefono -eq 0 ] ; then
+    echo " $amigo inexistente" 
+   else
+    echo "el telefono de $amigo es $telefono"
+ fi
