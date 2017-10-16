3817118c39e04d810458c8770ddd441a9e1b45cd
paniix <32488186+paniix@users.noreply.github.com>
2017-10-16 16:50:46 +0200

    Revisado Andrea-Patricia

--- FernandoSalcedo.sh
+++ FernandoSalcedo.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 #Script pide contraseña al usuario
-#Revisado por Andrea_Rioja y Patricia_Davila,correcto
+#Revisado por Andrea_Rioja y Patricia_Davila,Correcto
 #Palabra clave: if (Mira si se cumple la condicion)
 
 echo "Bienvenido, para ver configuración de red escriba contraseña"
