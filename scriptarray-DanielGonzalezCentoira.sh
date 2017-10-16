d9a0894cace1677767227ab1e3458d4b19275744
mbelanabel <mbelanabel@gmail.com>
2017-10-16 15:55:44 +0200

    Update scriptarray-DanielGonzalezCentoira.sh

--- scriptarray-DanielGonzalezCentoira.sh
+++ scriptarray-DanielGonzalezCentoira.sh
@@ -1,5 +1,16 @@
-#Script que crea dos arrays, uno con el nombre detus 6  mejores amigos y otro con sus telefonos#!bin/bash
+#Script que crea dos arrays, uno con el nombre detus 6  mejores amigos y otro con sus telefonos
+#!bin/bash
 #DanielGonzálezCentoira
+# rev. comentarios porJuan T.
+#
+# keywords for (bucle que pasa por todos los valores del array), declare -a "definir array"
+# for : toma los valores (primer valor= usuariio "nombres de los amigos"; segundo valor= numero
+# "números de teléfono")
+# for -> valor (*) "todos los elementos del array"
+# variables: valor - auxiliar cambia (nombres y números)
+# contador: índice (/no será necesario ; cuando se pida amigo (índice del array); se podría 
+# utilizar para ello.
+#
 clear
 contador=1
 declare -a usuario=( Sergio Jorge Juan Antonio David Alvaro )
