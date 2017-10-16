21aff8a2d76680cbf25ed8ae9fa927ca6a8143ef
fllopez20 <32488258+fllopez20@users.noreply.github.com>
2017-10-16 15:31:28 +0200

    Update Felipe Lobato.sh

--- Felipe Lobato.sh
+++ Felipe Lobato.sh
@@ -1,14 +1,24 @@
-#!/bins/bash
-echo "MENU"
-echo "1) Saber cuantos ficheros de dispositivo de bloques hay en /dev"
-echo "2) Ver cuales son y lo que ocupan los perfiles de usuario"
-echo "3) Crear un informe con esa informacion"
-echo "4) Salir"
-
-echo "Introduzca una opcion"
+#! /bin/bash
+echo MENU
+echo 1 Saber cuantos ficheros de dispositivo tipo caracter hay en dev
+echo 2 Ver cuales son y lo que ocupan los perfiles de usuario
+echo 3 Crear informe con esa informacion
+echo 4 salir
+echo "Introduce una opcion"
 read opcion
+echo "La opcion seleccionada es $opcion"
 case $opcion in
-	1) echo "El numero de ficheros que hay son"
-	   ls -f | more
+
+1) echo "Vamos a ver los ficheros que hay" 
+	ls -lrt /dev/ | cut -c 1 | grep "c" | wc -l
+			;;
+2) echo "2 vamos a ver cuales son y lo que ocupan los perfiles de usuario"
+	cat /etc/passwd |wc -c
+	    ;;
+3) echo "3 Creamos un informe con la informacion"
+		 ls -lrt /dev/ | cut -c 1 | grep "c" | wc -l >> informefichero.txt 
+		cat /etc/passwd | wc -c  >> informefichero.txt
 		;;
-esac
+4) echo "4 Salir"
+	;;
+esac 
