c58d8440d963f8ac506b7b782924086bf2baaf3a
jtondadi <32275181+jtondadi@users.noreply.github.com>
2017-10-17 01:56:02 +0200

    Update scriptarray-DanielGonzalezCentoira.sh

--- scriptarray-DanielGonzalezCentoira.sh
+++ scriptarray-DanielGonzalezCentoira.sh
@@ -9,20 +9,20 @@
 # for -> valor (*) "todos los elementos del array"
 # variables: valor - auxiliar cambia (nombres y números)
 # contador: índice (/no será necesario ; cuando se pida amigo (índice del array); se podría 
-# utilizar para ello.
-#
+# utilizar para ello. localización del índice posteriormente 
+# 
 clear
-contador=1
+# contador=1
 declare -a usuario=( Sergio Jorge Juan Antonio David Alvaro )
 declare -a numero=( 669 848 776 565 443 221 )
 for valor in ${usuario[*]}
 do
 	echo "El usuario $contador vale $valor"
-	contador=`expr $contador + 1`
+#	contador=`expr $contador + 1`
 done
-contador=1
+# contador=1
 for valor in ${numero[*]}
 do
 	echo "El numero $contador vale $valor"
-	contador=`expr $contador + 1`
+#	contador=`expr $contador + 1`
 done
