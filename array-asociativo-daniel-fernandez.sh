caf2693c5103b2ca05cac873601bd5ce6852094c
jtondadi <32275181+jtondadi@users.noreply.github.com>
2017-10-17 02:32:45 +0200

    Update array-asociativo-daniel-fernandez.sh

--- array-asociativo-daniel-fernandez.sh
+++ array-asociativo-daniel-fernandez.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+# author: Daniel Fernández
+# revisión: Juan Tonda
+# keywords: break (saltar la condición "if"; ya no continua evaluando)
+# modificación de la estructura IF para verificar la existencia o no del amigo
+#
 clear
-
 declare -A numeros 
-
 numeros=([dela]="658" [ruben]="686" [nerea]="666" [patri]="621")
-
 for nombre in "${!numeros[@]}"
-do
+   do
 	numero=${numeros[$nombre]}
 	echo "El usuario $nombre tiene el numero $numero"
-done
+   done
 read -p "Intoducir amigo a consultar: " amigo
-if [ $numeros=$amigo ]; then 
-	echo "El telefono de $amigo es $numero"
-fi
 
+for nombre in "${!numeros[@]}" 
+   do
+     numero=${numeros[$nombre]}
+     if [ $amigo = $nombre ]; then 
+	echo "El telefono de $amigo es $numero"
+        op=0
+        break # saltar la condición, cuando encuentra el amigo
+       else
+        op=1
+     fi
+   done
+ 
+      if [ $op -eq 1 ]; then
+        echo "Has introducido el amigo $amigo, que no se encuentra en la lista"
+      fi
