ba66cd6a25a0759a1d40feeb4cab8f4a0ff78a89
jtondadi <32275181+jtondadi@users.noreply.github.com>
2017-10-17 02:00:34 +0200

    Update secuencias_escape.sh

--- secuencias_escape.sh
+++ secuencias_escape.sh
@@ -18,23 +18,23 @@ echo -e "introduce valor 1 \c"; read var1
 echo -e "introduce valor 2 \c"; read var2
 echo -e "introduce valor 3 \c"; read var3
 if test "$var1" = "$var2" -a "$var1" = "$var3"
-then
-echo los tres valores son iguales
-else
-if [ "$var1" = "$var2" ]
-then
-echo "los dos primeros valores son iguales"
-else
-if [ "$var1" = "$var3" ]
-then
-echo "el primer y tercer valor son iguales"
-else
-if [ "$var2" = "$var3" ]
-then
-echo "los dos últimos valores iguales"
-else
-echo "ningún valor igual"
-fi
-fi
-fi
+  then
+   echo los tres valores son iguales
+  else
+   if [ "$var1" = "$var2" ]
+    then
+     echo "los dos primeros valores son iguales"
+    else
+        if [ "$var1" = "$var3" ]
+         then
+          echo "el primer y tercer valor son iguales"
+         else
+           if [ "$var2" = "$var3" ]
+            then
+             echo "los dos últimos valores iguales"
+            else
+             echo "ningún valor igual"
+            fi
+        fi
+    fi
 fi
