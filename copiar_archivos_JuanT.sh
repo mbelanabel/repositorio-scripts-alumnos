849ecf6dddf8796399c84ce69145ef60f1f869d3
jtondadi <32275181+jtondadi@users.noreply.github.com>
2017-10-18 23:47:06 +0200

    Add files via upload

--- copiar_archivos_JuanT.sh
+++ copiar_archivos_JuanT.sh
@@ -1,39 +1,52 @@
 # jt. octubre 2017 
-# comprobar la existencia o no de un fichero
-# si existe: BORRAR
-# si no existe: CREAR
+# pasar 2 archivos como parametros 
+# si existe el archivo origen y no existe el destino: copiar ORIGEN a DESTINO
+# si no existe el archivo origen: mostrar mensaje de error y salir
+# si existe el archivo destino:   mostrar mensaje de error y salir 
 #
-dir=`pwd`
+
+# función error. para sacar el tipo de error, por el mensaje
+error() {
+ clear 
+ echo "*** ERROR EN LA EJECUCIÓN DEL SCRIPT ****"
+ echo "Tipo de error: " $mensaje
+ read -p "Pulsa una tecla para continuar " pausa
+}
+
+dir=`pwd` #para poder seguir quedando en el mismo directorio de ejecución
 clear
+
+# comprobar el paso de parámetros
+op=0
 if test $# -lt 1
  then
-   echo "ERROR -----------------------------------"
-   read -p "No has pasado ningún parametro !!  " pausa
-   op=0
+  mensaje=" NO HAN SIDO PASADO PARAMETROS " 
+  error
   elif test $# -eq 1
    then 
-    echo "error. solo has pasado un único archivo "
-    read -p "Pulsa una tecla para continuar ..." pausa
+    mensaje=" HA SIDO PASADO UN ÚNICO ARCHIVO " 
+    error
   elif test $# -eq 2
     then 
      op=1
-     echo "ok. tengo 2 parametros"
-     read -p "Pulsa una tecla para continuar ..." pausa
  fi
 
+# comprobar la existencia de los archivos
 if [ $op -eq 1 ]; then
  if test -a $1    
   then 
-   echo "El fichero origen existe" 
      if test -a $2
        then 
-         echo "Destino existente. no se puede copiar"
+         mensaje="Destino existente. No se puede copiar: $2"
+         error
        else 
-         echo "copiando $1 en $2"
+         clear
+         echo " ** Copiando $1 en $2 **"
          cp $1 $2
     fi
   else 
-   echo "FICHERO origen inexistente"
+   mensaje="FICHERO origen inexistente: $1"
+   error
  fi
 fi
 
