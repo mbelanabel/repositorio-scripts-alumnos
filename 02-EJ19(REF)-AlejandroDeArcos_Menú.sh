03fd43ab71a24c70d3ed0619802ab8044d3475b9
Darcos90 <32488185+Darcos90@users.noreply.github.com>
2017-10-29 19:08:13 +0100

    Create 02-EJ19(REF)-AlejandroDeArcos_Menú.sh

---
+++ 02-EJ19(REF)-AlejandroDeArcos_Menú.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+clear
+echo __Menú__
+echo " "
+echo "1) Crear un grupo."
+echo "2) Crear un usuario."
+echo "3) Saber cuántos ficheros hay en /mnt."
+echo "4) Listar los ficheros y 10 segundos después, meter ese listado en un f$
+echo "5) Salir."
+read -p "elija una opción:" opcion
+echo "la opcion elegida es $opcion"
+case $opcion in
+        1) echo "Se va a crear un grupo"
+           read -p "Introduzca el nombre del grupo: " grupo
+           sudo addgroup $grupo
+           echo "Se ha creado el grupo $grupo en el sistema"
+        ;;
+        2) echo "Se va a crear un usuario"
+           read -p "Introduzca el nombre del usuario: " usuario
+           sudo adduser $usuario
+           echo "Se ha creado el usuario $usuario en el sistema"
+        ;;
+        3) echo "Se va a comprobar cuantos ficheros hay en /mnt"
+           echo "Hay `ls -al /mnt | wc -l` ficheros en el directorio /mnt"
+        ;;
+        4) echo "Se van a listar los ficheros para su posterior almacenamient$
+           ls -l
+           sleep 10
+           ls -l >> fichContados.txt
+           echo "Información guardada en fichContados.txt"
+        ;;
+        5) echo "Gracias por usar este script"
+        ;;
+        *) echo "opción incorrecta"
+        ;;
+esac
