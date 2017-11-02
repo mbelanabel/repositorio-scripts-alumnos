853e7265c89ea4fc16b6114b54d74fd9077714ec
Darcos90 <32488185+Darcos90@users.noreply.github.com>
2017-11-02 13:05:18 +0100

    Update 02-AGENDA_AlejandrodeArcos.sh

--- 02-AGENDA_AlejandrodeArcos.sh
+++ 02-AGENDA_AlejandrodeArcos.sh
@@ -57,8 +57,48 @@ case $opcion in
 #			;;
 #			*) echo "Opción incorrecta"
 #			;;
-#	    esac
-;;
+        3) echo "Se va a proceder a la edición de un contacto"
+           echo "¿Esta seguro de editar un contacto?"
+           echo "1 Si (Continuar editando el contacto deseado)"
+           echo "2 No (Salir del programa sin efectuar cambios)"
+           echo " "
+           echo "Elija una opción"
+           read opcion
+           case $opcion in
+                        1 |SI|S|si|s|YES|Y|yes|y)
+			echo "Contactos a modificar"
+			cat ~/agenda.txt | nl
+			echo "Escriba nombre apellido mote direccion nºtel email para editar el campo deseado: "
+                        read -p "Introduzca un dato: " dato1 dato2
+                        sed -i s/$dato1/$dato2/g ~/agenda.txt
+			echo "El contacto se ha modificado correctamente"
+			echo " "
+                        cat ~/agenda.txt
+                        ;;
+                        2 |NO|N|no|n) echo "Operación cancelada, Ha salido de la agenda (Ejecute de nuevo)"
+                        ;;
+                        *) echo "Opción incorrecta"
+                        ;;
+           esac
+           ;;
+	4)echo "Se va a realizar la busqueda del contacto"
+	  echo "¿Está seguro de realizar la busqueda?"
+	  echo "1 Si, estoy seguro"
+	  echo "2 No, Salir de la agenda"
+	  echo " "
+	  echo "Eliga una opción"
+	  read opcion
+	  case $opcion in
+			1 |SI|S|si|s|YES|Y|yes|y)
+                        read -p "Introduzca algun dato como nombre, mote, direccion para realizar la búsqueda: " dato
+                        grep -e $dato ~/agenda.txt
+                        ;;
+                        2 |NO|N|no|n) echo "Operación cancelada, Ha salido de la agenda (Ejecute de nuevo)"
+                        ;;
+                        *) echo "Opción incorrecta"
+               		;;
+	  esac
+	  ;;
 	0) echo "Ha salido de la agenda"
 	;;
 	*) echo "Opción incorrecta, elija una opción válida"
