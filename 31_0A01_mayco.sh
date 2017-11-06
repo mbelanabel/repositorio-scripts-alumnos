1fc595744df0561467c4eec052376bed8827ef6d
jtondadi <32275181+jtondadi@users.noreply.github.com>
2017-11-06 09:57:11 +0100

    Update 31_0A01_mayco.sh

--- 31_0A01_mayco.sh
+++ 31_0A01_mayco.sh
@@ -1,4 +1,4 @@
-#!/home/alumno
+#!bin/bash
 #Redacta un script que compruebe si esta en el directorio /, si es así muestra los permisos que tienen los ficheros que contiene, si no, cambia de directorio /.
 #Autor Mayco Costa
 #Revisado por Andrea Rioja, Daniel Fernández
@@ -12,6 +12,7 @@ if [ $directorio = "/" ] ; then
 #Aquí esta diciendo si estoy en el directorio o no, ya que sino es porque antes tengo que situarme en el directorio / para que funcione.
  	echo "Si estas en el directorio $directorio "
         ls -l 
+        cd $directorio // vuelves a donde está el script
 else
  	echo "No estas en el directorio $directorio "
 fi
