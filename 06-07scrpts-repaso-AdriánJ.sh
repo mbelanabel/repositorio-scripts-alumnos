978d4437a43250f377f476082ff9b0150cacd6d2
Adrijc <32488156+Adrijc@users.noreply.github.com>
2018-01-15 21:51:09 +0100

    Delete 06-07scrpts-repaso-AdriánJ.sh

--- 06-07scrpts-repaso-AdriánJ.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Adrián Jarne
-#Script que comprueba si una contraseña dada por el usuario es correcta, si es asi, vuelca la configuración de las interfaces de red en un fichero.
-#Elementos clave: date, calculos fechas
-nombre=Adrián
-echo "Mi nombre es $nombre"
-cal 10 1989
-dia=$(date --date "Oct 3, 1989" +%A) #Creo la variable dia en la que guardo el calculo del dia de la semana (--date -> meter una fecha en concreto, +%A -> formato de fecha que muestra dia de la semana)
-echo "Nací el $dia"
