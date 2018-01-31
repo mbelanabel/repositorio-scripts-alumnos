c93d676ce0a454de64ffc4111d0a6cacc1bb913f
Danyels0310 <32487560+Danyels0310@users.noreply.github.com>
2018-01-31 18:31:03 +0100

    Update 22_numerar_DaniFP.sh

--- 22_numerar_DaniFP.sh
+++ 22_numerar_DaniFP.sh
@@ -2,14 +2,12 @@
 #Escribe un script que devuelva el numero de ficheros de mas de 1Mb dado
 #un directorio
 clear
-buscar (){
+
 read -p "Intoduce un directorio: " dir
-}
-buscar
 if [ -z $dir ]; then
 	echo "No has escrito ningun directorio"
 elif  [ -d $dir ]; then
-	find $dir -type f -size +1000k -exec ls -l {} \; | wc -l
+	find $dir -type f -size +1000k -exec wc -l {} \;
 elid [ -f $dir ]; then
 	echo "Has escrito un archivo y no un directorio"
 fi
