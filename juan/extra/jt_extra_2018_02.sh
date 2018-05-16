b5fae0960d6a311a9f9352f15ea82b3c4547dfdc
jtondadi <32275181+jtondadi@users.noreply.github.com>
2018-05-16 02:35:14 +0200

    Delete jt_extra_2018_02.sh

--- juan/extra/jt_extra_2018_02.sh
+++
@@ -1,6 +0,0 @@
-# 2018
-# jt. partiendo de Anabel
-# 02 - Ídem pero que en vez de "Lucas" muestre los parámetros introducidos ('adios02').
-#!/bin/bash
-# Muestra por pantalla todos los atributos del comando
-echo "Hasta luego $@"
