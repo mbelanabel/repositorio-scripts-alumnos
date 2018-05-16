949cce47906d961f4c698de2ebb7e2719ee9d33e
jtondadi <32275181+jtondadi@users.noreply.github.com>
2018-05-16 02:32:36 +0200

    Delete jt_extra_2018_01.sh

--- juan/extra/jt_extra_2018_01.sh
+++
@@ -1,6 +0,0 @@
-# 2018
-# jt. partiendo del archivo de Anabel
-# 01 - Realizar un script llamado 'adios01' que muestre por pantalla “Hasta luego Lucas”.
-#!/bin/bash
-# Muestra por pantalla "hasta luego lucas"
-echo "Hasta luego lucas"
